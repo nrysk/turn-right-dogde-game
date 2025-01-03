c7b4f2de81c03153c202f30ce2fe54d45df86c60
ry05k2ulv <ryosuke.10117@gmail.com>
2025-01-03 23:25:38 +0900

    ✨ CooltimeBar の実装

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Player(pg.sprite.Sprite):
     COLOR = (150, 150, 150)
     COOLTIME = 30
     SPEED = 3
+    MAX_HEALTH = 200
 
     def __init__(self, *groups):
         super().__init__(*groups)
@@ -22,7 +23,7 @@ class Player(pg.sprite.Sprite):
         self.rect.center = (WIDTH // 2, HEIGHT // 2)
         self.cooltime = 0
         self.direction = 0  # 0:上, 1:右, 2:下, 3:左
-        self.health = 100
+        self.health = Player.MAX_HEALTH
 
     def update(self):
         self.cooltime = max(0, self.cooltime - 1)
@@ -45,14 +46,29 @@ class Player(pg.sprite.Sprite):
 
 
 class HealthBar:
-    def __init__(self, player):
+    def __init__(self, player, rect):
         self.player = player
-        self.rect = pg.Rect(10, 10, 100, 15)
+        self.rect = pg.Rect(rect)
+        self.max_width = self.rect.width
 
     def draw(self, screen):
         pg.draw.rect(screen, (255, 0, 0), self.rect)
-        self.rect.width = self.player.health
+        self.rect.width = self.player.health / Player.MAX_HEALTH * self.max_width
         pg.draw.rect(screen, (0, 255, 0), self.rect)
+
+
+class CooltimeBar:
+    def __init__(self, player, rect):
+        self.player = player
+        self.rect = pg.Rect(rect)
+        self.max_width = self.rect.width
+
+    def draw(self, screen):
+        pg.draw.rect(screen, (120, 120, 120), self.rect)
+        self.rect.width = (
+            (Player.COOLTIME - self.player.cooltime) / Player.COOLTIME * self.max_width
+        )
+        pg.draw.rect(screen, (180, 180, 180), self.rect)
 
 
 class Enemy(pg.sprite.Sprite):
@@ -102,7 +118,8 @@ def main():
     all_sprites.add(enemies)
 
     # UI の初期化
-    health_bar = HealthBar(player)
+    health_bar = HealthBar(player, (10, 10, 200, 15))
+    cooltime_bar = CooltimeBar(player, (10, 30, 200, 8))
 
     # 敵のスポーンタイマーの設定
     pg.time.set_timer(USEREVENT_SPAWNENEMY, 1000)
@@ -128,6 +145,7 @@ def main():
         screen.fill(BACKGROUND_COLOR)
         all_sprites.draw(screen)
         health_bar.draw(screen)
+        cooltime_bar.draw(screen)
         pg.display.flip()
         clock.tick(60)
 
