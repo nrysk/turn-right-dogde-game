63c98bdcecae5bae50d0a5a5f2e0dd7610df621d
ry05k2ulv <ryosuke.10117@gmail.com>
2025-01-03 22:19:26 +0900

    ✨ Window の表示

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import pygame as pg
+
+WIDTH, HEIGHT = 400, 600
+BACKGROUND_COLOR = (240, 240, 240)
+
+
 def main():
-    print("Hello from turn-right-dogde-game!")
+    # 初期化
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    pg.display.set_caption("Turn Right Dogde Game")
+    clock = pg.time.Clock()
+
+    # ゲームループ
+    running = True
+    while running:
+        # イベント処理
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        # 画面更新
+        screen.fill(BACKGROUND_COLOR)
+        pg.display.flip()
+        clock.tick(60)
+
+    # 終了処理
+    pg.quit()
 
 
 if __name__ == "__main__":
